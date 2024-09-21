3552105de82fc45a7ea1ea68566d9ca853d6b400
Victor-Gabriel-Toledo <victorgabrielolv18@gmail.com>
2024-09-21 12:48:12 -0300

    atualização do projeto

--- Rankeada.js
+++ Rankeada.js
@@ -1,34 +1,30 @@
-rankeada()
-
-
-function rankeada(vitorias = 200, derrotas = 11, raking){
+function rankeada(vitorias, derrotas){
     let soma = vitorias - derrotas
+    let raking
     if(soma <= 10){
         raking = "Ferro"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
     else if (soma >= 11 && soma < 21){
         raking = "Bronze"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
     else if (soma >= 21 && soma < 51){
         raking = "Prata"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
     else if (soma >= 51 && soma < 80){
         raking = "Ouro"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
     else if (soma >= 80 && soma < 90){
         raking = "Diamante"
-        console.log (`O herói tem saldo de ${vitorias} vitótias e seu nível é ${raking}`)
     }
     else if (soma >= 90 && soma < 100){
         raking = "Lendário"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
     else {
         raking = "Imortal"
-        console.log (`O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`)
     }
+
+    return `O herói tem saldo de ${vitorias} vitórias e seu nível é ${raking}`
 }
+
+let resultado = rankeada(200, 11)
+console.log(resultado)
